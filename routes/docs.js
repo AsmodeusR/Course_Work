995376c6d0bdb82207eb88e7626a1ac04da727dd
AsmodeusR <62301451+AsmodeusR@users.noreply.github.com>
2020-05-27 15:32:46 +0300

    SAD12

--- routes/docs.js
+++ routes/docs.js
@@ -10,18 +10,18 @@ router.route("/").get((req, res) => {
 router.route("/add").post((req, res) => {
   const name = req.body.name;
   const text = req.body.text;
-  if (Document.exists({ name: name })) {
-    Document.findOneAndUpdate({ name: name }, { text: text })
-      .then(() => res.json("Document updated!"))
-      .catch((err) => res.status(400).json("Error: " + err));
-  } else {
-    const newDocument = new Document({
-      name,
-      text,
-    });
+  //if (Document.exists({ name: name })) {
+  Document.findOneAndUpdate({ name: name }, { text: text })
+    .then(() => res.json("Document updated!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+  //} else {
+  const newDocument = new Document({
+    name,
+    text,
+  });
 
-    newDocument.save().catch((err) => res.status(400).json("Error: " + err));
-  }
+  newDocument.save().catch((err) => res.status(400).json("Error: " + err));
+  //}
 });
 
 router.route("/:name").delete((req, res) => {
